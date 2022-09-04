a34a1a5147febd795d11a86de6aad25c43eb2ed2
Alexander Kulikovskiy <90175511+AlexKulikovskiy@users.noreply.github.com>
2022-09-05 01:40:58 +0300

    slider

---
+++ main/Pulse/src/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        prevArrow: '<button type="button" class="prev-slick"><img src="../icons/chevron-left-solid.png"></img></button>',
+        nextArrow: '<button type="button" class="next-slick"><img src="../icons/chevron-right-solid.png"></img></button>'
+    });
+  });
