00c6954728232736733d75c21f62acff99853782
Alexander Kulikovskiy <90175511+AlexKulikovskiy@users.noreply.github.com>
2022-09-09 11:01:39 +0300

    New

--- main/Pulse/src/js/script.js
+++ main/Pulse/src/js/script.js
@@ -1,6 +1,56 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
-        prevArrow: '<button type="button" class="prev-slick"><img src="../icons/chevron-left-solid.png"></img></button>',
-        nextArrow: '<button type="button" class="next-slick"><img src="../icons/chevron-right-solid.png"></img></button>'
-    });
+  
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
+
+  function toggleSlide(item) {
+      $(item).each(function(i) {
+          $(this).on('click', function(e) {
+              e.preventDefault();
+              $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+              $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          })
+      });
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+  var slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    nav: false,
+    controls: false
+  });
+
+document.querySelector('.next').onclick = function () {
+    slider.goTo('next');
+};
+document.querySelector('.prev').onclick = function () {
+    slider.goTo('prev');
+  };
+  
+// modal
+
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+});
+$('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
+
+$('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+    });
+});
+});
+
+
+
